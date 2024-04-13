f984cafd96fb9600333c933ef918410876311a41
Takahiro Ido <juve.taka@gmail.com>
2024-04-13 14:50:20 +0000

    add: link,thumbnails, pdf file

--- js/app.js
+++ js/app.js
@@ -1,23 +1,6 @@
 
 // "load" event: https://developer.mozilla.org/ja/docs/Web/API/Window/load_event
 window.addEventListener("load", (event) => {
-    // request = new XMLHttpRequest();
-    // request.open("GET", "json/portfolio.json");
-    // request.responseType = "json";
-    // request.send();
-    // request.onload = function() {
-    //     const portfolio_hashmap_arr = JSON.parse(JSON.stringify(request.response));
-    //     console.log('xhr data:%o', portfolio_hashmap_arr);
-    //     console.log('typeof xhr data:%o', typeof portfolio_hashmap_arr);
-    // };
-    //     for (var i = 0; i < Object.keys(portfolio_hashmap_arr).length; i++) {
-    //         render_portfolio(i,portfolio_hashmap_arr,document.getElementById("portfolio"));
-    //     }
-
-    //     for (var i = 0; i < Object.keys(portfolio_hashmap_arr).length; i++) {
-    //         render_table(i,portfolio_hashmap_arr,document.getElementById("table_of_contents"));
-    //     }
-    // }
     // https://qiita.com/uhyo/items/91649e260165b35fecd7
     fetch("json/portfolio.json", {method: "GET"})
     .then(response => response.json())
@@ -39,7 +22,7 @@ function render_table(index,portfolio_hashmap_arr,table){
     const list_link_str = "#portfolio"+portfolio_hashmap_arr[index].id;
 
     // list_index.innerHTML = portfolio_hashmap_arr[index].id < 10 ? "0"+portfolio_hashmap_arr[index].id : portfolio_hashmap_arr[index].id;
-    list_index.innerHTML = portfolio_hashmap_arr[index].id;
+    list_index.innerHTML = portfolio_hashmap_arr[index].id + ". ";
     list_link.href = list_link_str;
     list_link.textContent = portfolio_hashmap_arr[index].title;
 
@@ -54,16 +37,16 @@ function render_table(index,portfolio_hashmap_arr,table){
 
 function render_portfolio(index,portfolio_hashmap_arr,portfolio){
     const div = document.createElement("div");
-    const title = document.createElement("h3");
+    const title = document.createElement("h2");
     const summary = document.createElement("h4");
     const thumbnail = document.createElement("img");
-    const langage = document.createElement("h3");
+    const langage = document.createElement("h4");
     const langage_body = document.createElement("p");
     const border = document.createElement("p");
-    const content = document.createElement("h3");
+    const content = document.createElement("h4");
     const content_body = document.createElement("p");
     const btn_group = document.createElement("div");
-    const btn_view = document.createElement("button");
+    // const btn_view = document.createElement("button");
     const btn_source = document.createElement("button");
 
     div.setAttribute("id", "portfolio"+portfolio_hashmap_arr[index].id);
@@ -81,28 +64,28 @@ function render_portfolio(index,portfolio_hashmap_arr,portfolio){
     thumbnail.classList.add("img-size","m-3");
     border.classList.add("border-bottom","border-2","border-secondary");
     btn_group.classList.add("btn-group","mb-5");
-    btn_view.classList.add("btn","btn-sm","btn-outline-secondary");
+    // btn_view.classList.add("btn","btn-sm","btn-outline-secondary");
     btn_source.classList.add("btn","btn-sm","btn-outline-secondary");
 
-    btn_view.type = "button";            
-    btn_view.textContent = 'View';
-    btn_view.onclick = view;
+    // btn_view.type = "button";
+    // btn_view.textContent = 'View';
+    // btn_view.onclick = view;
 
     btn_source.type = "button";
     btn_source.textContent = 'Code';
     btn_source.onclick = code;
 
-    function view(){
-        window.location.href = portfolio_hashmap_arr[index].view;
-    }
+    // function view(){
+    //     window.location.href = portfolio_hashmap_arr[index].view;
+    // }
 
     function code(){
         window.location.href = portfolio_hashmap_arr[index].source;
     }
 
-    if(portfolio_hashmap_arr[index].view !== ""){
-        btn_group.appendChild(btn_view);
-    }
+    // if(portfolio_hashmap_arr[index].view !== ""){
+    //     btn_group.appendChild(btn_view);
+    // }
     btn_group.appendChild(btn_source);
 
     div.append(title);
